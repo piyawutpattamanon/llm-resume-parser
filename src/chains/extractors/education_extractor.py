4b5d85f0a039971fd125fbb7b2ff5e2d3e157dea
Sam Narak <samnarak@gmail.com>
2024-05-07 23:20:55 +0700

    formatted education

--- src/chains/extractors/education_extractor.py
+++ src/chains/extractors/education_extractor.py
@@ -1,24 +1,42 @@
 import json
-from langchain_core.runnables import RunnableLambda, RunnableSequence
+from typing import List
+from pydantic import BaseModel, Field
+from langchain_core.runnables import RunnableLambda
 from langchain_core.runnables.retry import RunnableRetry
-from langchain.prompts.chat import ChatPromptTemplate
+from langchain.output_parsers import PydanticOutputParser
+from langchain_core.prompts import PromptTemplate
 from src.chains.extractors.base import Base
-from src.utils.extract_json import extract_json_only
+
+
+class Education(BaseModel):
+    city: str | None = Field(description="The city of the education", default=None)
+    school: str | None = Field(description="The school name")
+    country: str | None = Field(description="The country of the education", default=None)
+    end_date: str | None = Field(description="The end date of the education")
+    start_date: str | None = Field(description="The start date of the education")
+    degree_name: str | None = Field(description="The name of the degree", default=None)
+    description: str | None = Field(description="Additional description of the education", default=None)
+    country_code: str | None = Field(description="The country code of the education", default=None)
+    degree_major: str | None = Field(description="The major of the degree", default=None)
+
+
+class EducationExperience(BaseModel):
+    education: List[Education]
+
+
 class EducationExtractor(Base):
     def __init__(self, llm):
         self.llm = llm
         self.chain = None
 
-    def validate(self, text):
-        result = extract_json_only(text)
-        if len(result) == 0:
-            raise ValueError("No job position found in the resume")
-        return result[0]
-    
     def get_fallback_value(self, text):
-        return "this is a fixed value"
-    
+        return '{"error": "this is a fixed value"}'
+
     def get_prompt_template(self):
+        parser = PydanticOutputParser(pydantic_object=EducationExperience)
+
+        format_instructions = parser.get_format_instructions()
+
         prompt_text = """
 this is a resume
 =====
@@ -26,58 +44,35 @@ this is a resume
 =====
 you are resume parser.
 extract his education as specified in his resume.
-output in json
-
-example output
-[
-  {{
-    "city": "",
-    "school": "State University",
-    "country": "",
-    "end_date": "2024-5",
-    "start_date": "-",
-    "degree_name": "",
-    "description": "",
-    "country_code": "NC",
-    "degree_major": ""
-  }},
-  {{
-    "city": "",
-    "school": "Central Piedmont Community College",
-    "country": "",
-    "end_date": "2021-12",
-    "start_date": "2020-8",
-    "degree_name": "",
-    "description": "North Carolina Agricultural and Technical State University, Greensboro, NC\nMay 2024\nGPA: 3.4\nCentral Piedmont Community College, Charlotte NC\nAugust 2020 - December 2021\nGPA: 3.3\nCourse Careers, Remote December 2022 - January 2023\nGPA: 3.3\nCourse Careers, Remote December 2022 - January 2023",
-    "country_code": "NC",
-    "degree_major": ""
-  }}
-]
-
-if there is no education in his resume, output a blank array like this
-[ ]
-    """
-        prompt = ChatPromptTemplate.from_template(prompt_text)
-        return prompt
+{format_instructions}
+"""
+        prompt = PromptTemplate(
+            template=prompt_text,
+            input_variables=["resume_content"],
+            partial_variables={"format_instructions": format_instructions},
+        )
+        return prompt | self.llm | parser
 
     def build_chain(self):
         minprompt = self.get_prompt_template()
 
         safe_min = RunnableRetry(
-            bound=minprompt | self.llm | RunnableLambda(self.validate),
+            bound=minprompt,
             max_attempt_number=2,
         ).with_fallbacks(
             [RunnableLambda(self.get_fallback_value)]
         )
 
-        return safe_min
+        formatted_chain = safe_min | RunnableLambda(lambda result: result.dict())
 
+        return formatted_chain
 
     def get_chain(self):
         if self.chain is None:
             self.chain = self.build_chain()
         return self.chain
-    
+
+
 if __name__ == '__main__':
     resumes = []
     with open('Entity Recognition in Resumes.jsonl') as f:
@@ -98,6 +93,4 @@ if __name__ == '__main__':
     result = chain.invoke({'resume_content': resumes[0]['content']})
 
     print('=== clean result ===')
-    print(json.dumps(json.loads(result), indent=4))
-    print('=== raw result ===')
-    print(result)
+    print(json.dumps(result, indent=4))
