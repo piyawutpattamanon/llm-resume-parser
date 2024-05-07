e59178bc3ac82b568b0ef89bd32fef14773a59f0
Sam Narak <samnarak@gmail.com>
2024-05-07 23:28:04 +0700

    formatted person extractor

--- src/chains/extractors/person_extractor.py
+++ src/chains/extractors/person_extractor.py
@@ -1,62 +1,69 @@
 import json
+from typing import List
+from pydantic import BaseModel, Field
 from langchain_core.runnables import RunnableLambda, RunnableSequence
 from langchain_core.runnables.retry import RunnableRetry
-from langchain.prompts.chat import ChatPromptTemplate
+from langchain.prompts import PromptTemplate
+from langchain.output_parsers import PydanticOutputParser
 from src.chains.extractors.base import Base
-from src.utils.extract_json import extract_json_only
+
+class Person(BaseModel):
+    gender: str|None = Field(description="The gender of the person")
+    birthplace: str|None = Field(description="The birthplace of the person")
+    first_name: str|None = Field(description="The first name of the person")
+    family_name: str|None = Field(description="The family name of the person")
+    middle_name: str|None = Field(description="The middle name of the person")
+    nationality: str|None = Field(description="The nationality of the person")
+    date_of_birth: str|None = Field(description="The date of birth of the person")
+    marital_status: str|None = Field(description="The marital status of the person")
+    country_code: str|None = Field(description="The country code of the person")
+
+
 class PersonExtractor(Base):
     def __init__(self, llm):
         self.llm = llm
         self.chain = None
-
-    def validate(self, text):
-        result = extract_json_only(text)
-        if len(result) == 0:
-            raise ValueError("No job position found in the resume")
-        return result[0]
     
     def get_fallback_value(self, text):
         return '{"error": "this is a fixed value"}'
     
     def get_prompt_template(self):
+        parser = PydanticOutputParser(pydantic_object=Person)
+
+        format_instructions = parser.get_format_instructions()
+
         prompt_text = """
 this is a resume
 =====
 {resume_content}
 =====
-what's the name of this person? what country he is from? is he married? should he be male or female?
+what's the name of this person who is the owner of this resume?
+what country he/she is from? is he/she married? should he/she be male or female?
 output in json. no explanation. no comment
 
-example output
-[
-    {{
-        "gender": "",
-        "birthplace": "Thailand",
-        "first_name": "Hello",
-        "family_name": "World",
-        "middle_name": "",
-        "nationality": 'Thai',
-        "date_of_birth": "",
-        "marital_status": "",
-        "country_code": "TH"
-    }}
-]
+{format_instructions}
 """
-        prompt = ChatPromptTemplate.from_template(prompt_text)
-        return prompt
+        prompt = PromptTemplate(
+            template=prompt_text,
+            input_variables=["resume_content"],
+            partial_variables={"format_instructions": format_instructions},
+        )
+
+        return prompt | self.llm | parser
 
     def build_chain(self):
         minprompt = self.get_prompt_template()
 
         safe_min = RunnableRetry(
-            bound=minprompt | self.llm | RunnableLambda(self.validate),
+            bound=minprompt,
             max_attempt_number=2,
         ).with_fallbacks(
             [RunnableLambda(self.get_fallback_value)]
         )
 
-        return safe_min
+        formatted_chain = safe_min | RunnableLambda(lambda result: result.dict())
 
+        return formatted_chain
 
     def get_chain(self):
         if self.chain is None:
@@ -87,9 +94,9 @@ if __name__ == '__main__':
     result = chain.invoke({'resume_content': resumes[0]['content']})
 
     print('=== clean result ===')
-    print(json.dumps(json.loads(result), indent=4))
-    print('=== raw result ===')
-    print(result)
+    print(json.dumps(result, indent=4))
+    # print('=== raw result ===')
+    # print(result)
 
     end_time = datetime.datetime.now()
     print(f'Time taken: {end_time - start_time}')
