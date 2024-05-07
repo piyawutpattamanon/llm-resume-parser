b6499e8d37853e367f88b60ea6527cbba5a2188f
Sam Narak <samnarak@gmail.com>
2024-05-08 00:22:29 +0700

    split in chunks

--- src/chains/main.py
+++ src/chains/main.py
@@ -1,8 +1,10 @@
 import json
+from typing import List
 from langchain_core.runnables import RunnableParallel, RunnableLambda, RunnableSequence
 from langchain_core.runnables.retry import RunnableRetry
 from langchain.prompts.chat import ChatPromptTemplate
 from langchain_core.runnables import RunnableConfig
+from langchain_text_splitters import TokenTextSplitter
 
 from src.chains.extractors.education_extractor import EducationExtractor
 from src.chains.extractors.job_extractor import JobExtractor
@@ -15,7 +17,13 @@ class ResumeParserChain:
         self.llm = llm
         self.chain = None
 
-    def build_chain(self):
+    def split_chunks(self, resume_content) -> List[str]:
+        text_splitter = TokenTextSplitter(chunk_size=1500, chunk_overlap=500)
+        chunks = text_splitter.split_text(resume_content)
+
+        return chunks
+
+    def get_single_chunk_chain(self):
         if self.chain is None:
 
             education_chain = EducationExtractor(llm=self.llm).get_chain()
@@ -46,8 +54,27 @@ class ResumeParserChain:
 
 
     def parse(self, resume_content):
-        chain = self.build_chain()
-        return chain.invoke({
+        chunks = self.split_chunks(resume_content)
+
+        print('number of chunks', len(chunks))
+        input('press to continue')
+
+        single_chunk_chain = self.get_single_chunk_chain()
+
+        combined_chain = RunnableParallel(
+            {
+                f'chunk_{chunk_index}': single_chunk_chain
+                for chunk_index
+                in range(len(chunks))
+            }
+        )
+
+        # def combine_chunks(src):
+
+
+        
+
+        return combined_chain.invoke({
             "resume_content": resume_content
         })
     
@@ -66,7 +93,7 @@ if __name__ == '__main__':
     set_debug(True)
 
     resume_paser = ResumeParserChain(llm=llm)
-    result = resume_paser.parse(resumes[0])
+    result = resume_paser.parse(resumes[0]['content'])
 
     # print('=== raw result ===')
     # print(result)
